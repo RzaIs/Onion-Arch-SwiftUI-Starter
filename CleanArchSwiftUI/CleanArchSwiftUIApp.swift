db4594eed658943844d2a913d763bed366fdac6e
IsRza <108722676+IsRza@users.noreply.github.com>
2022-08-21 23:19:25 +0400

    Initial Commit

---
+++ CleanArchSwiftUI/CleanArchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchSwiftUIApp.swift
+//  CleanArchSwiftUI
+//
+//  Created by Rza Ismayilov on 21.08.22.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanArchSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CleanArchSwiftUI
+//
+//  Created by Rza Ismayilov on 21.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
